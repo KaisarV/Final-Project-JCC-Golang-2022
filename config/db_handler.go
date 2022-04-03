e2eda5dbd16431dd7a0cfd41f51a9ca63d6894f6
KaisarV <kaisar.valentino123@gmail.com>
2022-04-03 16:57:51 +0700

    fix db_handler

--- config/db_handler.go
+++ config/db_handler.go
@@ -3,20 +3,18 @@ package controllers
 import (
 	"Final-Project-JCC-Golang-2022/utils"
 	"database/sql"
-	"fmt"
 	"log"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
-	"gorm.io/driver/mysql"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func Connect() *sql.DB {
 	environment := utils.Getenv("ENVIRONMENT", "development")
-	fmt.Println("aaaaaaaaaaaaaaaaaaaaaaaaa" + environment)
+
 	if environment == "production" {
 		username := os.Getenv("DATABASE_USERNAME")
 		password := os.Getenv("DATABASE_PASSWORD")
@@ -40,14 +38,14 @@ func Connect() *sql.DB {
 	} else {
 		db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/db_tes_jcc?charset=utf8mb4&parseTime=True&loc=Local")
 
-		username := "root"
-		password := ""
-		host := "tcp(127.0.0.1:3306)"
-		database := "db_tes_jcc"
+		// username := "root"
+		// password := ""
+		// host := "tcp(127.0.0.1:3306)"
+		// database := "db_tes_jcc"
 
-		dsn := fmt.Sprintf("%v:%v@%v/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, database)
-		db2, _ := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-		db2.AutoMigrate(&User{}, &Transaction{}, &Store{}, &Cart{}, &Product{}, &ProductReview{}, &Feedback{})
+		// dsn := fmt.Sprintf("%v:%v@%v/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, database)
+		// db2, _ := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		// db2.AutoMigrate(&User{}, &Transaction{}, &Store{}, &Cart{}, &Product{}, &ProductReview{}, &Feedback{})
 
 		if err != nil {
 			log.Fatal(err)
